608f8caedad1c4e21fbde5f0dbc69268eb9cb077
Михаил Петров <mikhailpetrov80@gmail.com>
2022-07-17 11:57:45 +0400

    Decision 17.07.22

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.servise.ServiceCar;
 import web.servise.ServiceCarImpl;
 
 import java.util.ArrayList;
@@ -13,7 +14,7 @@ import java.util.List;
 @Controller
 public class CarController {
 
-    ServiceCarImpl serviceCar = new ServiceCarImpl();
+    private final ServiceCar serviceCar = new ServiceCarImpl();
 
     @GetMapping(value = "/cars")
     public String printCar(@RequestParam(value = "count", required = false) Integer count, ModelMap carModel) {
